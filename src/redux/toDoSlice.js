f3b7879034ecc6eca351ce0af8ee8915ac933b6c
Victor Shaviya <vs610642@digicelgroup.com>
2023-02-13 16:44:26 -0500

    Use createSlice to create todo action and reducer and import the same to the store for management

---
+++ src/redux/toDoSlice.js
@@ -0,0 +1,26 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const toDoSlice = createSlice({
+  
+  name: 'todos',
+  initialState: [
+    { id: 1, title: 'todo1', completed: true },
+    { id: 2, title: 'todo2', completed: false },
+    { id: 3, title: 'todo3', completed: true }
+  ],
+  reducers: {
+    addTodo: (state, action) => {
+      const newTodo = {
+        id: Date.now(),
+        title: action.payload.title,
+        completed: false
+      };
+      state.push(newTodo);
+    },
+  },
+
+});
+
+export const { addTodo } = toDoSlice.actions;
+
+export default toDoSlice.reducer;
