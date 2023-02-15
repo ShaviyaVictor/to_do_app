e7bb6824c677026eba18e6db28bbeaef6de061b1
shaviyavictor <shaviyavictor@gmail.com>
2023-02-15 17:35:10 -0500

    Custom edit the display

--- src/redux/toDoSlice.js
+++ src/redux/toDoSlice.js
@@ -4,9 +4,11 @@ const toDoSlice = createSlice({
   
   name: 'todos',
   initialState: [
-    { id: 1, title: 'todo1', completed: true },
-    { id: 2, title: 'todo2', completed: false },
-    { id: 3, title: 'todo3', completed: true }
+    { id: 1, title: 'Todo1', completed: false },
+    { id: 2, title: 'Todo2', completed: true },
+    { id: 3, title: 'Todo3', completed: false },
+    { id: 4, title: 'Todo4', completed: true },
+    { id: 5, title: 'Todo5', completed: false },
   ],
   reducers: {
 
