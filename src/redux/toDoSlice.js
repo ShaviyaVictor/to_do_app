8a0485e213ac4851176a11ceeec35c196498a241
shaviyavictor <shaviyavictor@gmail.com>
2023-02-17 19:25:53 -0500

    Comment out the hard coded todos for better tracking

--- src/redux/toDoSlice.js
+++ src/redux/toDoSlice.js
@@ -17,7 +17,13 @@ export const getTodosAsync = createAsyncThunk(
 const toDoSlice = createSlice({
   
   name: 'todos',
-  initialState: [ ],
+  initialState: [
+    // { id: 1, title: 'Todo1', completed: false },
+    // { id: 2, title: 'Todo2', completed: true },
+    // { id: 3, title: 'Todo3', completed: false },
+    // { id: 4, title: 'Todo4', completed: true },
+    // { id: 5, title: 'Todo5', completed: false },
+  ],
   reducers: {
 
     addTodo: (state, action) => {
@@ -41,7 +47,7 @@ const toDoSlice = createSlice({
     },
 
   },
-
+  
   // thunk reducer logic
   extraReducers: {
 
