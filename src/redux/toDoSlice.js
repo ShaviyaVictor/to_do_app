bafd0416c87d21a09e651141fa9d7a4cb0773ffb
Victor Shaviya <vs610642@digicelgroup.com>
2023-02-20 15:54:26 -0500

    Create the action to handle the fulfilled results of the respective reducers

--- src/redux/toDoSlice.js
+++ src/redux/toDoSlice.js
@@ -116,6 +116,13 @@ const toDoSlice = createSlice({
       state.push(action.payload.todo);
     },
 
+    [toggleCompleteAsync.fulfilled]: (state, action) => {
+      const index = state.findIndex(
+        (todo) => todo.id === action.payload.id
+      );
+      state[index].completed = action.payload.completed;
+    },
+
   },
 
 });
