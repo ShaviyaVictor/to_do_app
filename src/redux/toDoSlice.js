9eecbc0d36f08fd251503223b2b49575c3693e04
Victor Shaviya <vs610642@digicelgroup.com>
2023-02-21 13:27:37 -0500

    Uncomment the Initial todos for site visibility but to view the functionalities, follow steps in the README to see how it works with the API

--- src/redux/toDoSlice.js
+++ src/redux/toDoSlice.js
@@ -89,11 +89,11 @@ const toDoSlice = createSlice({
   
   name: 'todos',
   initialState: [
-    // { id: 1, title: 'Todo1', completed: false },
-    // { id: 2, title: 'Todo2', completed: true },
-    // { id: 3, title: 'Todo3', completed: false },
-    // { id: 4, title: 'Todo4', completed: true },
-    // { id: 5, title: 'Todo5', completed: false },
+    { id: 1, title: 'Todo1', completed: false },
+    { id: 2, title: 'Todo2', completed: true },
+    { id: 3, title: 'Todo3', completed: false },
+    { id: 4, title: 'Todo4', completed: true },
+    { id: 5, title: 'Todo5', completed: false },
   ],
   // reducers: {
 
